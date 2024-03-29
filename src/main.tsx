73f70278125a8aed00f0671ab5264bdb729e1159
Santiago Cañas Zapata <santiagocanaszapata@Santiagos-Mac-mini.local>
2024-03-28 19:12:32 -0500

    WIP shopping cart

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { ShoppingCartProvider } from "./User/Context/ShoppingCartContext.tsx";
 
-// import './index.css'
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <ShoppingCartProvider>
     <BrowserRouter>
